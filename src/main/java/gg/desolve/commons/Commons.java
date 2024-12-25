8f04d72554baf56d91f31978b7313dde0e390faa
Michele <michele47801@gmail.com>
2024-12-25 07:11:40 +0000

    Add static instance getter

--- src/main/java/gg/desolve/commons/Commons.java
+++ src/main/java/gg/desolve/commons/Commons.java
@@ -1,12 +1,16 @@
 package gg.desolve.commons;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Commons extends JavaPlugin {
 
+    @Getter
+    public static Commons instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
