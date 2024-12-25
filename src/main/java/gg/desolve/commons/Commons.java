c16ae0e8e450ffee3865aa29c397f4a284b6c0f9
Michele <michele47801@gmail.com>
2024-12-25 19:58:42 +0000

    Add configuration and Redis manager initialization

--- src/main/java/gg/desolve/commons/Commons.java
+++ src/main/java/gg/desolve/commons/Commons.java
@@ -1,5 +1,8 @@
 package gg.desolve.commons;
 
+import gg.desolve.commons.config.Config;
+import gg.desolve.commons.config.ConfigurationManager;
+import gg.desolve.commons.redis.RedisManager;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,14 +11,27 @@ public final class Commons extends JavaPlugin {
     @Getter
     public static Commons instance;
 
+    @Getter
+    public ConfigurationManager configurationManager;
+
+    @Getter
+    public RedisManager redisManager;
+
+
     @Override
     public void onEnable() {
         instance = this;
 
+        configurationManager = new ConfigurationManager(this, "language.yml", "storage.yml");
+        redisManager = new RedisManager();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public Config getConfig(String name) {
+        return configurationManager.getConfig(name);
     }
 }
