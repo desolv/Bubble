c71fdc7237540e9112a1a6cbf2e48c95bd9353b7
Michele <michele47801@gmail.com>
2024-12-25 19:59:36 +0000

    Utility with relevant helpful functionality

---
+++ src/main/java/gg/desolve/commons/relevance/Converter.java
@@ -0,0 +1,15 @@
+package gg.desolve.commons.relevance;
+
+import java.util.UUID;
+
+public class Converter {
+
+    public static String generateId() {
+        String randomId = UUID.randomUUID().toString()
+                .replaceAll("-", "")
+                .toLowerCase();
+        return randomId.substring(0, Math.min(12, randomId.length()));
+    }
+
+
+}
