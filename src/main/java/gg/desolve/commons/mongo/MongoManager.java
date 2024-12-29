31d94a929b1442f7dfb9bd63a375675d3f1a8612
Michele <michele47801@gmail.com>
2024-12-29 19:31:25 +0000

    Add MongoDB integration with MongoManager

---
+++ src/main/java/gg/desolve/commons/mongo/MongoManager.java
@@ -0,0 +1,36 @@
+package gg.desolve.commons.mongo;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import gg.desolve.commons.Commons;
+import lombok.Getter;
+import org.bson.UuidRepresentation;
+
+@Getter
+public class MongoManager {
+
+    private MongoDatabase mongoDatabase;
+    private MongoClient mongoClient;
+
+    public MongoManager(String url, String database) {
+        try {
+            MongoClientSettings mongoSettings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(url))
+                    .uuidRepresentation(UuidRepresentation.STANDARD)
+                    .build();
+
+            mongoClient = MongoClients.create(mongoSettings);
+            mongoDatabase = mongoClient.getDatabase(database);
+
+            String timing = String.valueOf(System.currentTimeMillis() - Commons.getInstance().getInstanceManager().getInstance().getBooting());
+            Commons.getInstance().getLogger().info("Merged Mongo @ " + timing + "ms.");
+        } catch (Exception e) {
+            Commons.getInstance().getLogger().warning("There was a problem connecting to MongoDB.");
+            e.printStackTrace();
+        }
+    }
+
+}
