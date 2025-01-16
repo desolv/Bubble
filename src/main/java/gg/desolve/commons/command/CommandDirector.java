0e50530f00ea2447d0c39a29015ac2f67c45d206
Michele <michele47801@gmail.com>
2025-01-16 04:51:03 +0000

    Add context and completions for commands

--- src/main/java/gg/desolve/commons/command/CommandDirector.java
+++ src/main/java/gg/desolve/commons/command/CommandDirector.java
@@ -1,11 +1,20 @@
 package gg.desolve.commons.command;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.InvalidCommandArgument;
+import gg.desolve.commons.Commons;
 import gg.desolve.commons.command.management.InstanceCommand;
+import gg.desolve.commons.command.management.ScopeCommand;
+import gg.desolve.commons.instance.Instance;
+import gg.desolve.commons.relevance.Message;
+import gg.desolve.commons.scope.Scope;
 import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 public class CommandDirector {
@@ -15,17 +24,53 @@ public class CommandDirector {
 
     public CommandDirector(CommandManager commandManager) {
         this.commandManager = commandManager;
+        contexts();
+        completions();
         commands = commands();
     }
 
     private List<BaseCommand> commands() {
         List<BaseCommand> commandList = Arrays.asList(
-                new InstanceCommand()
+                new InstanceCommand(),
+                new ScopeCommand()
         );
 
         commandList.forEach(commandManager::registerCommand);
         return commandList;
     }
 
+    private void contexts() {
+        commandManager.getCommandContexts().registerContext(
+                Scope.class, s -> {
+                    String popName = s.popFirstArg();
+                    Scope scope = Commons.getInstance().getScopeManager().retrieve(popName);
+
+                    if (scope == null) {
+                        throw new InvalidCommandArgument(Message.translate("<red>Scope matching <yellow>" + popName + " <red>not found."), false);
+                    }
+
+                    return scope;
+                });
+
+        commandManager.getCommandContexts().registerContext(
+                Instance.class, i -> {
+                    String popName = i.popFirstArg();
+                    Instance instance = Commons.getInstance().getInstanceManager().retrieve(popName);
+                    return Optional.ofNullable(instance).orElseThrow(() ->
+                            new InvalidCommandArgument(Message.translate("<red>Instance matching <yellow>" + popName + " <red>not found."), false));
+                });
+    }
+
+
+    private void completions() {
+        commandManager.getCommandCompletions().registerAsyncCompletion("scopes", s ->
+                Stream.concat(
+                        Stream.of("global"),
+                        Commons.getInstance().getScopeManager().retrieve().stream().map(Scope::getName)
+                ).collect(Collectors.toList()));
+
+        commandManager.getCommandCompletions().registerAsyncCompletion("instances", s ->
+                Commons.getInstance().getInstanceManager().retrieve().stream().map(Instance::getName).toList());
+    }
 }
 
