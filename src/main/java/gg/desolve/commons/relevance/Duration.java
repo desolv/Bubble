31cf18feb69ad36e2e6c5c8916ae2a68b5e9b2e9
Michele <michele47801@gmail.com>
2025-01-18 06:16:07 +0000

    Convert to record class

--- src/main/java/gg/desolve/commons/relevance/Duration.java
+++ src/main/java/gg/desolve/commons/relevance/Duration.java
@@ -3,13 +3,7 @@ package gg.desolve.commons.relevance;
 import lombok.Getter;
 
 @Getter
-public class Duration {
-
-    private final long duration;
-
-    public Duration(long duration) {
-        this.duration = duration;
-    }
+public record Duration(long duration) {
 
     public static Duration duration(String duration) {
         long time = Converter.duration(duration);
