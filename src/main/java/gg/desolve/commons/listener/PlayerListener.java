062cf64c773c068e9061f8d2b573bedbf1d507c7
Michele <michele47801@gmail.com>
2025-01-20 04:12:44 +0000

    Add welcome motd and reboot information

--- src/main/java/gg/desolve/commons/listener/PlayerListener.java
+++ src/main/java/gg/desolve/commons/listener/PlayerListener.java
@@ -1,5 +1,10 @@
 package gg.desolve.commons.listener;
 
+import gg.desolve.commons.Commons;
+import gg.desolve.commons.instance.InstanceManager;
+import gg.desolve.commons.relevance.Converter;
+import gg.desolve.commons.relevance.Message;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,18 +14,39 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
-    @EventHandler
+    @EventHandler(
+            priority = EventPriority.HIGHEST
+    )
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.setJoinMessage(null);
+
+        Player player = event.getPlayer();
+        InstanceManager instanceManager = Commons.getInstance().getInstanceManager();
+
+        Message.send(player, Commons.getInstance().getLanguageConfig().getString("server.welcome_motd")
+                .replace("server%", Commons.getInstance().getLanguageConfig().getString("server.server_display")));
+
+        Message.send(player, Commons.getInstance().getLanguageConfig().getString("instance.welcome_instance_lifespan")
+                .replace("time%", Converter.time(System.currentTimeMillis() - instanceManager.getInstance().getBooting())),
+                "commons.admin");
+
+        Message.send(player, Commons.getInstance().getLanguageConfig().getString("instance.welcome_instance_schedule")
+                .replace("time%", Converter.time(Commons.getInstance().getRebootManager().getReboot().getAddedAt()
+                        + Commons.getInstance().getRebootManager().getReboot().getDelay() - System.currentTimeMillis())),
+                "commons.admin");
     }
 
-    @EventHandler
+    @EventHandler(
+            priority = EventPriority.HIGHEST
+    )
     public void onPlayerQuit(PlayerQuitEvent event) {
         event.setQuitMessage(null);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerQuit(AsyncPlayerChatEvent event) {
+    @EventHandler(
+            priority = EventPriority.HIGHEST
+    )
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
         event.setFormat(event.getPlayer().getName() + ": " + event.getMessage());
     }
 
