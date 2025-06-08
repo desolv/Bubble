1074c2f96892196852fbe070a5502c5f3ed04b18
Michele <michele47801@gmail.com>
2025-06-08 22:32:33 +0100

    Asking for name and making error throw exception

--- api/src/main/java/gg/desolve/api/blueprint/Logger.java
+++ api/src/main/java/gg/desolve/api/blueprint/Logger.java
@@ -1,19 +1,22 @@
 package gg.desolve.api.blueprint;
 
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 public class Logger {
 
+    public static String name;
+
     public static void info(String message) {
-        System.out.println("[Bubble] " + message);
+        System.out.println("[" + name + "] " + message);
     }
 
     public static void warning(String message) {
-        System.out.println("[Bubble] " + message);
+        System.out.println("[" + name + "] " + message);
     }
 
     public static void error(String message) {
-        System.err.println("[Bubble] " + message);
+        System.err.println("[" + name + "] " + message);
+        throw new IllegalStateException(message);
     }
 }
