dfb07ea6fd0eed0ee3164a9e603375e5455074b9
Michele <michele47801@gmail.com>
2025-06-08 21:53:55 +0100

    Added mongo manager

---
+++ api/src/main/java/gg/desolve/api/MongoManager.java
@@ -0,0 +1,36 @@
+package gg.desolve.api;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import gg.desolve.api.blueprint.Logger;
+import lombok.Getter;
+import org.bson.UuidRepresentation;
+
+@Getter
+public class MongoManager {
+
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+
+    public MongoManager(String url, String database) {
+        try {
+            long start = System.currentTimeMillis();
+
+            MongoClientSettings mongoSettings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(url))
+                    .uuidRepresentation(UuidRepresentation.STANDARD)
+                    .build();
+
+            mongoClient = MongoClients.create(mongoSettings);
+            mongoDatabase = mongoClient.getDatabase(database);
+
+            Logger.info("Connected to MongoDB in " + (System.currentTimeMillis() - start) + "ms.");
+        } catch (Exception e) {
+            Logger.warning("There was a problem connecting to MongoDB.");
+            e.printStackTrace();
+        }
+    }
+}
